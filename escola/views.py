2536da7808bc2aa81f392cdfa7f2f7ae05eadff9
Keven Andrade <nivekandrade@live.com>
2023-05-15 18:16:15 -0300

    adicionando autenticação

--- escola/views.py
+++ escola/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
@@ -8,16 +10,22 @@ class AlunosViewSet(viewsets.ModelViewSet):
     '''Exibindo alunos'''
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CursosViewsSet(viewsets.ModelViewSet):
     '''Exibindo todos os cursos'''
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class MatriculasViewSet(viewsets.ModelViewSet):
     '''Exibindo todas as matrículas'''
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ListaMatriculasAlunos(generics.ListAPIView):
     '''Listando as matriculas de um aluno'''
@@ -25,6 +33,8 @@ class ListaMatriculasAlunos(generics.ListAPIView):
         queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ListaAlunosMatriculados(generics.ListAPIView):
     '''Listando alunos matriculados em um curso'''
@@ -32,5 +42,7 @@ class ListaAlunosMatriculados(generics.ListAPIView):
         queryset = Matricula.objects.filter(curso_id= self.kwargs['pk'])
         return queryset
     serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
